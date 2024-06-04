1bb0d671aa3161b69850f68f062f73f519982176
Cainã Costa <me@cfcosta.com>
2024-06-04 12:53:01 -0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# µcrdt
+
+µcrdt is a collection of well-tested, well-documented [Conflict-Free Replicated Data Types (CRDTs)](https://en.wikipedia.org/wiki/Conflict-free_replicated_data_type).
+
+## License
+
+This project is licensed with either the [MIT](./LICENSE) or [Apache 2.0](./LICENSE-Apache2.0) licenses.
+
+If what you need to do is not covered by either of those licenses, please get in touch and we can make exceptions.
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A collection of well-tested CRDT implementations in Rust.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-dev-tools = {
+      url = "github:cfcosta/rust-dev-tools.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      rust-dev-tools,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-dev-tools.overlays.default ];
+        };
+
+        rdt = rust-dev-tools.setup pkgs {
+          name = "µ";
+          rust = rust-dev-tools.version.fromToolchainFile ./rust-toolchain.toml;
+          dependencies = with pkgs; [ ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ rdt.devShell ];
+          packages = with pkgs; [ ];
+        };
+      }
+    );
+}
