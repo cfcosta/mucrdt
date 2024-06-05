7e4b46ddf6f52a83e7c2e7150fa4e7483085011f
Cainã Costa <me@cfcosta.com>
2024-06-05 13:00:47 -0300

    minor(doc): document the methods on ToBytes

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,7 @@
 use proptest::prelude::*;
 
+pub use blake3::{Hash, Hasher};
+
 pub use crate::{
     error::*, impl_associate_bytes_types, prop_assert_changes, prop_assert_does_not_change,
     test_op_crdt_properties, test_state_crdt_properties, test_to_bytes, test_to_hex, testing::*,
@@ -24,18 +26,28 @@ where
 pub trait ToBytes {
     type Output: AsRef<[u8]>;
 
+    /// Converts the value to a representation in bytes.
     fn to_bytes(&self) -> Self::Output;
 
+    /// Converts the value to a representation in bytes, as a vector.
+    ///
+    /// This is a convenience method, and automatically derived from `to_bytes`.
     fn to_bytes_vec(&self) -> Vec<u8> {
         self.to_bytes().as_ref().to_vec()
     }
 
+    /// Hashes the value using the blake3 algorithm.
+    ///
+    /// This is a convenience method, and automatically derived from `to_bytes`.
     fn hash_bytes(&self) -> blake3::Hash {
         let mut hasher = blake3::Hasher::new();
         hasher.update(self.to_bytes().as_ref());
         hasher.finalize()
     }
 
+    /// Checks if the value (as bytes) is zero.
+    ///
+    /// This is useful for checking if a value is empty.
     fn is_zero(&self) -> bool {
         let len = self.to_bytes().as_ref().len();
         self.to_bytes_vec() == vec![0; len]
